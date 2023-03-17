608f685148781405998e9d19703806d47ef22031
Joseph C. Miller, II <josephcmiller2@gmail.com>
2023-03-17 12:55:48 -0600

    Create separate hosts file for named containers

--- run.sh
+++ run.sh
@@ -14,7 +14,11 @@ if [[ "$1" != "" ]]; then
         mkdir "${HOME}/.browser-${NAME}" "${HOME}/.browser-${NAME}/.cache" "${HOME}/.browser-${NAME}/.config" "${HOME}/.browser-${NAME}/.local" "${HOME}/.browser-${NAME}/.pki"
     fi
 
-    EXTRAARGS="${EXTRAARGS} -v ${HOME}/.browser-${NAME}/.cache:${GUESTUSERHOME}/.cache:rw -v ${HOME}/.browser-${NAME}/.config:${GUESTUSERHOME}/.config:rw -v ${HOME}/.browser-${NAME}/.local:${GUESTUSERHOME}/.local:rw -v ${HOME}/.browser-${NAME}/.pki:${GUESTUSERHOME}/.pki:rw"
+    if [[ ! -f "${HOME}/.browser-${NAME}/hosts" ]]; then
+        cp /etc/hosts "${HOME}/.browser-${NAME}/hosts"
+    fi
+
+    EXTRAARGS="${EXTRAARGS} -v ${HOME}/.browser-${NAME}/.cache:${GUESTUSERHOME}/.cache:rw -v ${HOME}/.browser-${NAME}/.config:${GUESTUSERHOME}/.config:rw -v ${HOME}/.browser-${NAME}/.local:${GUESTUSERHOME}/.local:rw -v ${HOME}/.browser-${NAME}/.pki:${GUESTUSERHOME}/.pki:rw  -v ${HOME}/.browser-${NAME}/hosts:/etc/hosts:rw"
 fi
 
 podman run ${RUNARGS} --hostname "${HOSTNAME}" --name "${NAME}" ${X11ARGS} ${FUSEARGS} ${SHAREDARGS} ${EXTRAARGS} "${TAGNAME}"
